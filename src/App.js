5dc441e2ea4c5bc7286725e67977b465eda705a2
StevenHa159 <stevenha@g.ucla.edu>
2025-07-11 22:28:39 -0700

    testing keyboard and restart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 import { words } from './words';
 import GameBoard from './components/GameBoard';
 import Keyboard from './components/Keyboard';
